29ccac507af7097785c3e115a5df033154cff838
Debart Nathan <denathan@laposte.net>
2023-07-12 09:57:47 +0200

    cleaning of original arborescence

--- controllers/api.ctrl.js
+++ controllers/api.ctrl.js
@@ -1,3 +1,1 @@
-exports.getTodos = (req, res) => res.end();
-exports.createTodo = (req, res) => res.end();
-exports.editTodo = (req, res) => res.end();
+
--- routing/api.routes.js
+++ routing/api.routes.js
@@ -1,12 +1,3 @@
 const router = require('express').Router();
-const {
-  getTodos,
-  createTodo,
-  editTodo
-} = require('../controllers/api.ctrl');
-
-router.get('/api/todos', getTodos);
-router.post('/api/todos', createTodo);
-router.put('/api/todos/:id', editTodo);
 
 module.exports = router;
