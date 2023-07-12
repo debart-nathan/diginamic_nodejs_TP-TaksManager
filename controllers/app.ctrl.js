588fb86db2fe33fe9e56329770b7c5c7594f3d32
Debart Nathan <denathan@laposte.net>
2023-07-12 12:28:06 +0200

    basic app ctrl

--- controllers/app.ctrl.js
+++ controllers/app.ctrl.js
@@ -1,1 +1,20 @@
 
+const {resolve} = require("path");
+
+exports.homeCtrl=(req,res)=>{
+  res.sendFile(resolve('public',"home.html"))
+}
+
+exports.userTasksListCtrl=(req,res)=>{
+  res.sendFile(resolve('public','html','userTasksList.html'));
+}
+
+exports.userTaskCreatorCtrl=(req,res)=>{
+  res.sendFile(resolve('public','html','userTaskCreator.html'));
+}
+
+exports.userTaskDetailsCtrl=(req,res)=>{
+  res.sendFile(resolve('public','html',"userTaskDetails.html"));
+}
+
+
