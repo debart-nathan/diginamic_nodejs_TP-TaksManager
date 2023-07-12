87f67de3f6a027c485ad0e830c27d3bfd80c6812
Debart Nathan <denathan@laposte.net>
2023-07-12 11:33:02 +0200

    basic app routing init

--- routing/app.routes.js
+++ routing/app.routes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+const {
+    userTaskDetailsCtrl,
+    userTaskCreatorCtrl,
+    userTasksListCtrl,
+    homeCtrl
+} = require("../controllers/app.ctrl.js")
+
+router.get("/user/:userId/task/:taskId", userTaskDetailsCtrl,);
+router.get("/user/:userId/addTask", userTaskCreatorCtrl);
+router.get("/user/:userId/tasks", userTasksListCtrl);
+router.get("/home",homeCtrl);
+
 module.exports = router;
