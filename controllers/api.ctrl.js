41e8cbab65afbb38cb97ca1e906f1a573f0c0dbd
Debart Nathan <denathan@laposte.net>
2023-07-13 03:00:11 +0200

    api : taskCreator : post task

--- controllers/api.ctrl.js
+++ controllers/api.ctrl.js
@@ -1,6 +1,7 @@
 const { resolve } = require("path");
+const { randomUUID } = require('crypto');
 const db = require("../db/db.json");
-const {updateJSON} = require("./utils/dataManip.js");
+const { updateJSON } = require("./utils/dataManip.js");
 
 exports.getUsersCtrl = (req, res) => {
     res.send(db.users);
@@ -18,21 +19,46 @@ exports.patchUserTaskDone = (req, res) => {
         return task.id == req.body.taskId;
     })
 
-    if(dbTaskI==-1){
+    if (dbTaskI == -1) {
         const er = new Error("ERROR 404 : The task couldn't be found in the database");
-        res.status(404).json({error:er})
+        res.status(404).json({ error: er })
         return;
     }
-    if(db.tasks[dbTaskI].userId!==req.body.userId){
+    if (db.tasks[dbTaskI].userId !== req.body.userId) {
         const er = new Error("ERROR 403 : You are not the User associated with this task");
-        res.status(403).json({error:er})
+        res.status(403).json({ error: er })
         return;
     }
-    
+
     db.tasks[dbTaskI].done = req.body.done
     updateJSON(db);
 
-    res.status ="200";
-    res.send(db.tasks[dbTaskI]);
+    res.status("200").send(db.tasks[dbTaskI]);
 }
 
+exports.postUserTask = (req, res) => {
+    const id = randomUUID() ;
+    const detailsId = randomUUID();
+        
+    const { description, ...rest } = req.body;
+    const task = {
+        id: id,
+        detailsId: detailsId,
+        ...rest
+    }
+    const details = {
+        id: detailsId,
+        taskId: id,
+        text: description,
+    }
+    if (db.users.find(user => user.id = task.userId) === undefined) {
+        const er = new Error("ERROR 404 : The task couldn't be found in the database");
+        res.status(404).json({ error: er })
+        return;
+    }
+    db.tasks.push(task);
+    db.taskDetails.push(details)
+    //updateJSON(db);
+
+    res.status(200).send()
+}
--- routing/api.routes.js
+++ routing/api.routes.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 const {
     getUsersCtrl,
     getUserTasksCtrl,
-    patchUserTaskDone
+    patchUserTaskDone,
+    postUserTask
 } = require("../controllers/api.ctrl.js")
 
 router.get("/api/users",getUsersCtrl);
 router.get("/api/user/:userId/tasks",getUserTasksCtrl);
-router.patch("/api/user/task/done",patchUserTaskDone)
+router.patch("/api/user/task/done",patchUserTaskDone);
+router.post("/api/user/task/add",postUserTask);
 
 module.exports = router;
